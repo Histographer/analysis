738188a8a35adb1c5888267c1c23d7e62549e901
Markus <marro98@gmail.com>
2020-02-11 22:06:31 +0100

    feat: implement ELO algorithm

--- src/histographer/analysis/ranking/algorithms.py
+++ src/histographer/analysis/ranking/algorithms.py
@@ -5,8 +5,7 @@ import numpy as np
 
 def balanced_rank_estimation(comparisons: List[Tuple[int, int]], n_objects: int):
     """
-    Returns a permutations of the integers in the range of n_objects which represents an ordering based on the given
-    pairwise comparisons.
+    Implements the BRE-algorithm to find an ordering based on pairwise comparisons
     :param comparisons: A list containing tuples representing comparisons of the form (winner, loser)
     :param n_objects: The number of different objects the comparisons are sampled from
     :return: A numpy array of integers between 0 and n_objects ranked by their favorability
@@ -14,4 +13,20 @@ def balanced_rank_estimation(comparisons: List[Tuple[int, int]], n_objects: int)
     relative_favorability = relative_favorability_from_comparisons(comparisons, n_objects)
     relative_favorability[relative_favorability.nonzero()] = 2 * relative_favorability[relative_favorability.nonzero()] - 1
     scores = np.sum(relative_favorability, axis=1)
+    return np.argsort(scores)
+
+
+def elo(comparisons: List[Tuple[int, int]], n_objects: int):
+    """
+    Implements the ELO-algorithm to find an ordering based on pairwise comparisons
+    :param comparisons: A list containing tuples representing comparisons of the form (winner, loser)
+    :param n_objects: The number of different objects the comparisons are sampled from
+    :return: A numpy array of integers between 0 and n_objects ranked by their favorability
+    """
+    scores = np.full(n_objects, 1000, dtype=float)
+    for winner, loser in comparisons:
+        prob_result = 1 / (1 + 1.0 * np.power(10, (scores[winner] - scores[loser]) / 400))
+        scores[winner] += (1 - prob_result) * 800 / len(comparisons)
+        scores[loser] -= prob_result * 800 / len(comparisons)
+
     return np.argsort(scores)
