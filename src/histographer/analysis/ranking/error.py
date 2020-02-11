fdea9bd270080b619a753e54e624dfc499ac5732
Markus <marro98@gmail.com>
2020-02-11 22:06:59 +0100

    feat: add ability to compare multiple algorithms in one plot

--- src/histographer/analysis/ranking/error.py
+++ src/histographer/analysis/ranking/error.py
@@ -1,4 +1,5 @@
 from src.histographer.analysis.ranking.mock import generate_mock_comparisons
+from typing import List
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -13,57 +14,63 @@ def calculate_error_norm(ranking: np.ndarray, norm: int = 2):
     return np.linalg.norm(ranking - np.arange(ranking.shape[0]), norm)
 
 
-def e_vs_error_rate(n_comparisons: int, n_objects: int, algorithm: staticmethod, repeats: int = 10, norm: int = 2):
+def e_vs_error_rate(n_comparisons: int, n_objects: int, algorithms: List[staticmethod], repeats: int = 10, norm: int = 2):
     """
     Plots the error as calculated by 'calculate_error_norm' against a number of different error rates
     :param n_comparisons: The number of pairwise comparisons performed
     :param n_objects: The number of different objects the comparisons are sampled from
-    :param algorithm: A function object designating the algorithm which is to be tested
+    :param algorithms: A list of function objects designating the algorithms which are to be tested
     :param repeats: The number of times a set of comparisons will be generated for a given error rate, as well as the
     the number of different error rates to be evaluated
     :param norm: The norm of the space used to calculate the error
     :return: Returns nothing, displays a graph
     """
-    errors = []
+    errors = [[] for _ in range(len(algorithms))]
     error_rates = np.linspace(0.01, 0.20, num=repeats)
     for error_rate in error_rates:
-        error = 0
-        for _ in range(repeats):
-            comparisons = generate_mock_comparisons(n_comparisons, n_objects, error_rate)
-            ranking = algorithm(comparisons, n_objects)
-            error += calculate_error_norm(ranking, norm)
-        errors.append(error / repeats)
+        for i, algorithm in enumerate(algorithms):
+            error = 0
+            for _ in range(repeats):
+                comparisons = generate_mock_comparisons(n_comparisons, n_objects, error_rate)
+                ranking = algorithm(comparisons, n_objects)
+                error += calculate_error_norm(ranking, norm)
+            errors[i].append(error / repeats)
 
-    plt.plot(error_rates, errors)
+    for i in range(len(algorithms)):
+        plt.plot(error_rates, errors[i])
+
     plt.show()
 
 
-def e_vs_n_comparisons(n_objects: int, error_rate: float, algorithm: staticmethod, repeats: int = 10, norm: int = 2):
+def e_vs_n_comparisons(n_objects: int, error_rate: float, algorithms: List[staticmethod], repeats: int = 10, norm: int = 2):
     """
     Plots the error as calculated by 'calculate_error_norm' against a number of different error rates
     :param n_objects: The number of different objects the comparisons are sampled from
     :param error_rate: The ratio of pairwise comparisons which to not reflect the 'true' permutation
-    :param algorithm: A function object designating the algorithm which is to be tested
+    :param algorithms: A list of function objects designating the algorithms which are to be tested
     :param repeats: The number of times a set of comparisons will be generated for a given number of comparisons,
     as well as the the number of different n_comparisons to be evaluated
     :param norm: The norm of the space used to calculate the error
     :return: Returns nothing, displays a graph
     """
-    errors = []
+    errors = [[] for _ in range(len(algorithms))]
     n_comparisonss = [int(x) for x in np.linspace(20, 1000, num=repeats)]
     for n_comparisons in n_comparisonss:
-        error = 0
-        for _ in range(repeats):
-            comparisons = generate_mock_comparisons(n_comparisons, n_objects, error_rate)
-            ranking = algorithm(comparisons, n_objects)
-            error += calculate_error_norm(ranking, norm)
-        errors.append(error / repeats)
+        for i, algorithm in enumerate(algorithms):
+            error = 0
+            for _ in range(repeats):
+                comparisons = generate_mock_comparisons(n_comparisons, n_objects, error_rate)
+                ranking = algorithm(comparisons, n_objects)
+                error += calculate_error_norm(ranking, norm)
+            errors[i].append(error / repeats)
 
-    plt.plot(n_comparisonss, errors)
+    for i in range(len(algorithms)):
+        plt.plot(n_comparisonss, errors[i])
+
     plt.show()
 
 
-def e_vs_n_objects(n_comparisons: int, error_rate: float, algorithm: staticmethod, repeats: int = 10, norm: int = 2):
+def e_vs_n_objects(n_comparisons: int, error_rate: float, algorithms: List[staticmethod], repeats: int = 10, norm: int = 2):
     """
     Plots the error as calculated by 'calculate_error_norm' against a number of different error rates
     :param n_comparisons: The number of pairwise comparisons performed
@@ -74,15 +81,18 @@ def e_vs_n_objects(n_comparisons: int, error_rate: float, algorithm: staticmetho
     :param norm: The norm of the space used to calculate the error
     :return: Returns nothing, displays a graph
     """
-    errors = []
+    errors = [[] for _ in range(len(algorithms))]
     n_objectss = [int(x) for x in np.linspace(5, 100, num=repeats)]
     for n_objects in n_objectss:
-        error = 0
-        for _ in range(repeats):
-            comparisons = generate_mock_comparisons(n_comparisons, n_objects, error_rate)
-            ranking = algorithm(comparisons, n_objects)
-            error += calculate_error_norm(ranking, norm)
-        errors.append(error / repeats)
+        for i, algorithm in enumerate(algorithms):
+            error = 0
+            for _ in range(repeats):
+                comparisons = generate_mock_comparisons(n_comparisons, n_objects, error_rate)
+                ranking = algorithm(comparisons, n_objects)
+                error += calculate_error_norm(ranking, norm)
+            errors[i].append(error / repeats)
 
-    plt.plot(n_objectss, errors)
+    for i in range(len(algorithms)):
+        plt.plot(n_objectss, errors[i])
+
     plt.show()
