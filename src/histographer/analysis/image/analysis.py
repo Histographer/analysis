63643cdf0c2bf25a04a1db9f89e5215d21d94c7e
marcelroed <marcelroed@gmail.com>
2020-04-08 12:07:20 +0200

    fix: Change structure of results

--- src/histographer/analysis/image/analysis.py
+++ src/histographer/analysis/image/analysis.py
@@ -8,20 +8,35 @@ from histographer.analysis.image.fetch import ImageData
 # results
 
 
+def _make_results(name, data):
+    new_dict = {'name': name}
+    if type(data) == dict:
+        new_dict['components'] = [_make_results(k, v) for k, v in data.items()]
+    else:
+        new_dict['val'] = data
+    return new_dict
+
+
 def he(im: ImageData):
     tissue, nuclei, no_class = im.segmentation
     mask, hed = im.mask, im.hed
 
-    return {
+    data = {
         'H': channel_metrics(ma.array(hed[..., 0], mask=(tissue & mask))),
         'E': channel_metrics(ma.array(hed[..., 1], mask=(nuclei & mask)))
     }
+
+    return _make_results('HE', data)
 
 
 def hsv(im: ImageData):
     hsv = rgb2hsv(im.rgb)
     mask = im.mask
-    return {
+
+    data = {
         channel_name: channel_metrics(ma.array(hsv[..., i], mask=mask))
         for i, channel_name in enumerate('HSV')
     }
+
+    return _make_results('HSV', data)
+
