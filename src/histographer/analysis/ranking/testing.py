931896cc5a7f2e95d6c8a72fe7c54c272df6f087
Markus <marro98@gmail.com>
2020-04-16 22:31:58 +0200

    feat: add skip test case

--- src/histographer/analysis/ranking/testing.py
+++ src/histographer/analysis/ranking/testing.py
@@ -1,6 +1,7 @@
 from src.histographer.analysis.ranking.algorithms import balanced_rank_estimation, elo, rank_centrality, iterate_active_elo, active_elo
 from src.histographer.analysis.ranking.mock import generate_mock_comparisons, generate_mock_comparisons_btl, save_dummy_gradient
 from src.histographer.analysis.ranking.error import e_vs_error_rate, e_vs_n_comparisons, e_vs_n_objects
+from src.histographer.analysis.ranking.high_level import suggest_pair
 from random import sample
 import numpy as np
 
@@ -8,4 +9,5 @@ N_COMPARISONS = 500
 N_OBJECTS = 10
 ERROR_RATE = 0.05
 
-save_dummy_gradient((0, 255, 0), (255, 255, 255), (512, 512), 16)
+suggestion = suggest_pair([0, 1, 2], [(1, 2), (1, 2), (1, 0), (1, 0)], [(0, 2)])
+print(suggestion)
